2228070da82c4d22542e681ca34180f3593cc6e1
cyberabis <babishek@gmail.com>
2015-05-22 16:25:47 +0530

    project init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onroadApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the onroadApp
+ */
+angular.module('onroadApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
