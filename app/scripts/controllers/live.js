986473b5100b8b83e9f862b19a137bbe6e033b45
cyberabis <babishek@gmail.com>
2015-06-08 11:51:37 +0530

    updates

--- app/scripts/controllers/live.js
+++ app/scripts/controllers/live.js
@@ -8,8 +8,12 @@
  * Controller of the onroadApp
  */
 angular.module('onroadApp')
-  .controller('LiveCtrl', function ($scope) {
+  .controller('LiveCtrl', function ($scope, uiGmapGoogleMapApi) {
 
-  	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+  	// uiGmapGoogleMapApi is a promise.
+    // The "then" callback function provides the google.maps object.
+    uiGmapGoogleMapApi.then(function(maps) {
+    	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    });
 
   });
